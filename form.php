c1ceeacc6ce609e4c9bb75b9b1069084c15f79f3
AbdulRehmanGHub <pakkhan9009@gmail.com>
2024-04-04 16:31:37 +0500

    completed styling and connections

--- form.php
+++ form.php
@@ -1,3 +1,7 @@
+<?php
+include ("connection.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +13,7 @@
 <body>
 
     <div class="container">
+        <form action="#" method="POST">
         <div class="title">
             Registration Form
         </div>
@@ -17,52 +22,52 @@
         
         <div class="input_field">
             <label for="fname">First Name</label>
-            <input type="text" class="input">
+            <input type="text" class="input" name="fname">
         </div>
 
         <div class="input_field">
             <label for="lname">Last Name</label>
-            <input type="text" class="input">
+            <input type="text" class="input"  name="lname">
         </div>
 
         <div class="input_field">
             <label for="password">Password</label>
-            <input type="password" class="input">
+            <input type="password" class="input"  name="password">
         </div>
 
         <div class="input_field">
             <label for="confirm_password">Confirm Password</label>
-            <input type="password" class="input">
+            <input type="password" class="input" name="con_password">
         </div>
 
         <div class="input_field">
             <label for="gender">Gender</label>
-            <div class="selectbox">
-            <select>
-                <option value="gender">Select</option>
-                <option value="gender">Male</option>
-                <option value="gender">Female</option>
+            <div class="custom_select">
+            <select name="gender">
+                <option value = "not selected">Select</option>
+                <option vlaue = "male">Male</option>
+                <option value = "female">Female</option>
             </select>
             </div>
         </div>
 
         <div class="input_field">
-            <label for="email">Email</label>
-            <input type="text" class="input">
+            <label for="email">Email Address</label>
+            <input type="text" class="input" name="email">
         </div>
 
         <div class="input_field">
-            <label for="phone">Phone</label>
-            <input type="text" class="input">
+            <label for="phone">Phone Number</label>
+            <input type="text" class="input" name="phone">
         </div>
 
         <div class="input_field">
             <label for="address">Address</label>
-            <textarea></textarea>
+            <textarea class="textarea"  name="address"></textarea>
         </div>
 
         <div class="input_field terms">
-            <label for="check" class="check">
+            <label class="check">
             <input type="checkbox">
             <span class="checkmark"></span>
             </label>
@@ -70,9 +75,34 @@
         </div>
 
         <div class="input_field">
-            <input type="submit" value="Register" class="btn">
+            <input type="submit" value="Register" class="btn" name="reg_btn">
         </div>
-
+        </div>
+        </form>
     </div>
 </body>
 </html>
+
+<!-- PHP Area -->
+<?php
+
+if(isset($_POST['reg_btn'])) {
+    $fname          = $_POST['fname'];
+    $lname          = $_POST['lname'];
+    $password       = $_POST['password'];
+    $con_password   = $_POST['con_password'];
+    $gender         = $_POST['gender'];
+    $email          = $_POST['email'];
+    $phone          = $_POST['phone'];
+    $address        = $_POST['address'];
+
+    $query = "INSERT INTO form VALUES ('$fname', '$lname', '$password', '$con_password', '$gender', '$email', '$phone', '$address')";
+    $data = mysqli_query($conn, $query);
+
+    if($data) {
+        echo "Data Inserted into db";
+    } else {
+        echo "Failed to insert data: " . mysqli_error($conn);
+    }
+}
+?>
